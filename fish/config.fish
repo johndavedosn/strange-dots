c502f8b45875b3e031a3831e87c10bfc85e1c33a
Ali moumen <moumenisbest@gmail.com>
2025-11-26 22:10:47 +0100

    Edited the prompt and its colors to a more purple/pink-ish theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,11 @@
 if status is-interactive
-	echo "Mr. Frank wants to say:"
-	fortune | cowsay -f tux
 	alias ll="ls -al"
 	alias rmall="rm -rf ./*"
-	alias get_quote="fortune | cowsay -f tux"
 	export CONFIG="$HOME/.config/"
 	abbr nv nvim
 	abbr tm tmux
 	abbr ff fastfetch
+	abbr cf cottonfetch
 	export NVIM_CONFIG="$HOME/.config/nvim/"
 	export FISH_CONFIG="$HOME/.config/fish/"
 	export PATH="$HOME/.local/bin:$PATH"
